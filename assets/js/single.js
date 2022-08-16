fb12167e9a893506f0ef41cad80a08ca7777bfe7
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-08-16 01:07:21 -0500

    created error handle for single.js

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,9 +1,27 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }  
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
         // request was successful 
         if (response.ok) {
@@ -17,11 +35,12 @@ var getRepoIssues = function(repo) {
                 }
             });
         } else {
-            console.log(response)
-            alert("There was a problem with your request!");
+           // if not successful, redirect to homepage
+           document.location.replace("./index.html");
         }
     });
 };
+
 
 var displayIssues = function(issues) {
 
@@ -33,7 +52,7 @@ var displayIssues = function(issues) {
     
     for (var i = 0; i < issues.length; i++) {
         // create a link element to make users to the issue on the github
-        var issueEl  = document.createElement('a');
+        var issueEl = document.createElement('a');
         issueEl.classList = "list-item flex-row justify-space-between align-center";
         issueEl.setAttribute("href", issues[i].html_url);
         issueEl.setAttribute("target", "_blank");
@@ -58,7 +77,7 @@ var displayIssues = function(issues) {
         issueEl.appendChild(typeEl);
         issueContainerEl.appendChild(issueEl);
     }
-};
+}; 
 
 var displayWarning = function(repo) {
     // add text to warning container
@@ -73,4 +92,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
